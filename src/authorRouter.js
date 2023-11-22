adc8574bf873a70f08ab239109b7c6c520492c10
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-11-22 11:27:03 +0100

    Strive Blog
    - Aggiunto il catch per gli errori nei metodi GET e POST;
    - Aggiunto il metodo PUT;

--- src/authorRouter.js
+++ src/authorRouter.js
@@ -7,7 +7,7 @@ const authorRouter = express.Router();
 // Test per provare l'endpoint
 authorRouter.get("/test", async (req, res) => {
     res.json({ message: "Authors router working!" });
-  });
+});
 
 // Metodo 'GET' per effettuare una richiesta di TUTTI gli autori
 authorRouter.get('/', async (req, res) => {
@@ -19,9 +19,9 @@ authorRouter.get('/', async (req, res) => {
 // Metodo 'GET' per effettuare una richiesta di un SOLO autore specifico
 authorRouter.get('/:id', async (req, res) => {
     const { id } = req.params
-    const author = await Author.findById({id})
+    const author = await Author.findById({ id })
 
-    if(!author){
+    if (!author) {
         return res.status(404).send()
     }
 
@@ -31,9 +31,30 @@ authorRouter.get('/:id', async (req, res) => {
 
 // Metodo 'POST' per AGGIUNGERE un autore
 authorRouter.post('/', async (req, res) => {
-    const newAuthor = new Author(req.body);
-    await newAuthor.save();
-    res.status(200).send(newAuthor)
+    try {
+        const newAuthor = new Author(req.body);
+        await newAuthor.save();
+        res.status(200).send(newAuthor)
+    } catch(error){
+        console.log(error)
+        res.status(400).send(error)
+    }
+    
+})
+
+
+// Metodo 'PUT' per MODIFICARE un autore
+authorRouter.put('/:id', async(req, res) => {
+    try{
+        const {id} = req.params
+        const authors = await Author.findByIdAndUpdate(id)
+        if(!authors){
+            return res.status(404).send()
+        }
+    } catch (error) {
+        console.log(error)
+        req.status(400).send(error)
+    }
 })
 
 export default authorRouter
