04a31f4e00616d2f29a9b140e95e0d734289b4f5
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-11-22 19:51:25 +0100

    StriveBlog
    - Aggiunto 'cors' per collegare il backend con il frontend;

--- src/apiRouter.js
+++ src/apiRouter.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import authorRouter from './authorRouter.js';
+import cors from 'cors'
 
 // Andiamo a mettere tutte le nostre chiamate CRUD in una sotto-directory (API)
 const apiRouter = express.Router()
 
+apiRouter.use(express.json())
+
 apiRouter.use('/authors', authorRouter)
+
+apiRouter.use(cors())
+
+apiRouter.options('*', cors())
 
 export default apiRouter
